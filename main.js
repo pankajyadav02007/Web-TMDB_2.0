4351a9abcb7bcac69de6736df48aefc801871015
pankaj <pankajyadav221306@gmail.com>
2025-09-09 18:41:03 +0530

    commit

--- main.js
+++ main.js
@@ -16,3 +16,123 @@ const headerOptions = {
 };
 
 // Find all Genres Movies List
+function getMovieGenres(){
+    fetch("https://api.themoviedb.org/3/genre/movie/list",{
+        method: "GET",
+        headers: headerOptions,
+    })
+    .then((res) => {
+        console.log(res);
+        if(!res.ok){
+throw new Error (`Response status: ${res.status}`) 
+        }
+
+        res
+        .json()
+        .then((data) => {
+            console.log(data);
+            movieGenresListEl.innerHTML = "";
+            for(let i = 0; i < data.genres.length; i++){
+                movieGenresListEl.innerHTML = movieGenresListEl.innerHTML + `<Option value="id=${data.genres[i].id}&name=${data.genres[i].name}">${data.genres[i].name}</Option>`;
+            }
+        })
+        .catch((err) => {
+            console.log(err);
+        });
+    })
+    .catch((err) => {
+        console.log(err);
+    });
+}
+
+// UpComing movies
+function getUpcomingMovieList(){
+    fetch("https://api.themoviedb.org/3/movie/upcoming",{
+        method: "GET",
+        headers: headerOptions,
+    })
+    .then((res) => {
+        console.log(res);
+        if(!res.ok){
+throw new Error (`Response status: ${res.status}`) 
+        }
+
+        res
+        .json()
+        .then((data) => {
+            console.log(data.results);
+
+            const upComingMovie = data.results || [];
+
+            const upComingMovieEl = document.getElementById("upComing-movie");
+
+            for(let i = 0; i < data.upComingMovie.length; i++){
+                upComingMovieEl.innerHTML = upComingMovieEl.innerHTML +
+                 `<figure class="movie-card">
+                <img src = "https://image.tmdb.org/t/p/w500${upComingMovie[i].poster_path}"/>
+                <figcaption class = "movie-info">
+                <h3>${upComingMovie[i].title}</h3>
+                <p>${upComingMovie[i].overview}</p>
+                <div class = "release-date">Release: ${upComingMovie[i].release-date}</div>
+                </figcaption>
+                </figure>`;
+            }
+        })
+        .catch((err) => {
+            console.log(err);
+        });
+    })
+    .catch((err) => {
+        console.log(err);
+    });
+}
+
+// movie list
+function getMovieList(catName, id){
+    fetch(`https://api.themoviedb.org/3/movie/${catName}`,{
+        method: "GET",
+        headers: headerOptions,
+    })
+    .then((res) => {
+        console.log(res);
+        if(!res.ok){
+throw new Error (`Response status: ${res.status}`) 
+        }
+
+        res
+        .json()
+        .then((data) => {
+            console.log(data.results);
+
+            const movies = data.results || [];
+
+            const movieConierEl = document.getElementById("upComing-movie");
+
+            for(let i = 0; i < movies.length; i++){
+                movieConierEl.innerHTML = movieConierEl.innerHTML +
+                 `<figure class="movie-card">
+                <img src = "https://image.tmdb.org/t/p/w500${movies[i].poster_path}"/>
+                <figcaption class = "movie-info">
+                <h3>${movies[i].title}</h3>
+                <p>${movies[i].overview}</p>
+                <div class = "release-date">Release: ${movies[i].release-date}</div>
+                </figcaption>
+                </figure>`;
+            }
+        })
+        .catch((err) => {
+            console.log(err);
+        });
+    })
+    .catch((err) => {
+        console.log(err);
+    });
+}
+
+
+window.addEventListener("", () => {
+getMovieGenres();
+getMoviesList("upcoming", "upcoming-movies")
+getMoviesList("populer", "populer-movies")
+getMoviesList("top_rated", "top-reted-movies")
+});
